9eac2891af3995ddd5baeae47997b18f38a2fb32
Jacob <jakubwiesniak102@gmail.com>
2024-12-01 14:41:02 +0100

    Edit Lab_5

--- Lab_5/main.py
+++ Lab_5/main.py
@@ -1,16 +1,79 @@
-# This is a sample Python script.
+import googleapiclient.discovery
+import pandas as pd
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+api_service_name = "youtube"
+api_version = "v3"
+DEVELOPER_KEY = "AIzaSyA3WzJGLAI8raiDJZfdKa9sdWcxBO6UqGs"
+
+youtube = googleapiclient.discovery.build(
+    api_service_name, api_version, developerKey=DEVELOPER_KEY)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def getcomments(video):
+    request = youtube.commentThreads().list(
+      part="snippet",
+      videoId=video,
+      maxResults=100
+    )
+
+    comments = []
+
+    # Execute the request.
+    response = request.execute()
+
+    # Get the comments from the response.
+    for item in response['items']:
+      comment = item['snippet']['topLevelComment']['snippet']
+      public = item['snippet']['isPublic']
+      comments.append([
+          comment['authorDisplayName'],
+          comment['publishedAt'],
+          comment['likeCount'],
+          comment['textOriginal'],
+          comment['videoId'],
+          public
+      ])
+
+    while True:
+        try:
+            nextPageToken = response['nextPageToken']
+        except KeyError:
+            break
+        nextPageToken = response['nextPageToken']
+        # Create a new request object with the next page token.
+        nextRequest = youtube.commentThreads().list(part="snippet", videoId=video, maxResults=100, pageToken=nextPageToken)
+        # Execute the next request.
+        response = nextRequest.execute()
+        # Get the comments from the next response.
+        for item in response['items']:
+          comment = item['snippet']['topLevelComment']['snippet']
+          public = item['snippet']['isPublic']
+          comments.append([
+              comment['authorDisplayName'],
+              comment['publishedAt'],
+              comment['likeCount'],
+              comment['textOriginal'],
+              comment['videoId'],
+              public
+          ])
+
+    df2 = comments
+    return df2
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def sentiment_analysis(comments, game):
+    sia = SentimentIntensityAnalyzer()
+    sentiments = {'neg': 0, 'neu': 0, 'pos': 0}
+    for comment in comments['text']:
+        temp = sia.polarity_scores(comment)
+        print(temp)
+        if temp['neg'] > temp['neu'] and temp['neg'] > temp['pos']:
+            sentiments['neg'] += 1
+        if temp['pos'] > temp['neu'] and temp['pos'] > temp['neg']:
+            sentiments['pos'] += 1
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    plt.bar(sentiments.keys(), sentiments.values())
+    plt.title(f"{game}")
